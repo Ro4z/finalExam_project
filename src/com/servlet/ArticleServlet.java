cbf830ac080f32f12be3b9375b4c544f2c487ea4
Ryu In Seong <insung1217@gmail.com>
2019-12-04 16:09:34 +0900

    ArticleServlet 추가 (본문 읽을 때 호출)

---
+++ src/com/servlet/ArticleServlet.java
@@ -0,0 +1,54 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class ArticleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ArticleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String author = request.getParameter("author");
+		String content = request.getParameter("content");
+		String title = request.getParameter("title");
+		
+		
+		session.setAttribute("author", author);
+		session.setAttribute("content", content);
+		session.setAttribute("title", title);
+		
+		
+		
+		response.sendRedirect("Article.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
